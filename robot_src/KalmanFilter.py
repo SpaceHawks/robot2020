4a8d146bb9f2781f55fc804399bdb5b1bf0b7ea9
camero2734 <42698419+camero2734@users.noreply.github.com>
2019-10-05 17:41:48 -0500

    Add adaptiveFilter

--- robot_src/KalmanFilter.py
+++ robot_src/KalmanFilter.py
@@ -8,9 +8,10 @@ matrix = numpy.matrix  # I just want my life to be easier
 
 
 class KalmanFilter:
-    def __init__(self, dt=1, stateMatrix=[0, 0, 0, 0, 0, 0], Q=0.0001, R=0.02):
+    def __init__(self, dt=1, stateMatrix=[0, 0, 0, 0, 0, 0], Q=0.0001, R=0.02, std=6):
         # Measurement time interval
         self.dt = dt
+
         # Initial values - these values don't have to be exact, just estimated
         self.X0 = matrix(stateMatrix).T  # State matrix
 
@@ -60,7 +61,18 @@ class KalmanFilter:
         self.R = matrix(numpy.eye(self.H.shape[0])) * R
 
         # Process error covariance
-        self.Q = matrix(numpy.eye(self.A.shape[0])) * Q
+        self.Q = matrix(numpy.diag([0, 0, 0, Q, Q, Q]))
+
+        # ADAPTIVE FILTER VARIABLES
+
+        # Number of standard deviations for adaptive filter to kick in
+        self.std = std
+
+        # Count for times Q has been incremented for the adaptive filter
+        self.count = 0
+
+        # How much to scale Q by for adaptive filter
+        self.Q_scale = 1000
 
     def addMeasurement(self, measurement, accVector):
         U = matrix(accVector)  # Control input vector
@@ -71,7 +83,20 @@ class KalmanFilter:
         K = P * self.HT * numpy.linalg.pinv(S)
         self.X0 = Xp + K * Y
         self.P0 = (self.I - K * self.H) * P
+
+        # Update Q if measurement was way off (or if the measurement has returned to more likely values)
+        # This should activate when the robot manuevers so that the model can correct itself quickly
+        self.runAdaptiveFilter(Y, S)
         return self.X0
+
+    def runAdaptiveFilter(self, Y, S):
+        E = Y.T * S.I * Y
+        if E > self.std:
+            self.Q *= self.Q_scale
+            self.count += 1
+        elif self.count > 0:
+            self.Q /= self.Q_scale
+            self.count -= 1
 
     def printValues(self):
         vals = self.X0.tolist()
