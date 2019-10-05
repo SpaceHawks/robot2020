c304a78e5593746a1526243d0897ecbec73ebbe8
camero2734 <42698419+camero2734@users.noreply.github.com>
2019-10-05 17:41:48 -0500

    Add adaptiveFilter
    Add Adaptive Filter Docs
    
    Add adaptive filtering images
    
    Update kalman.md
    
    Update default std value
    
    Added code to generate test graphs
    
    Add "adaptive" input to class
    
    Rename KalmanFilter.py to kalman.py
    
    Rename IMU.py to imu.py

--- robot_src/kalman.py
+++ robot_src/kalman.py
@@ -8,16 +8,17 @@ matrix = numpy.matrix  # I just want my life to be easier
 
 
 class KalmanFilter:
-    def __init__(self, dt=1, stateMatrix=[0, 0, 0, 0, 0, 0], Q=0.0001, R=0.02):
+    def __init__(self, dt=1, stateMatrix=[0., 0, 0, 0, 0, 0], Q=2, R=0.02, std=100, adaptive=True):
         # Measurement time interval
         self.dt = dt
+
         # Initial values - these values don't have to be exact, just estimated
         self.X0 = matrix(stateMatrix).T  # State matrix
 
         # State transition matrix
         self.A = matrix(
             [
-                [1, 0, 0, dt, 0, 0],
+                [1., 0, 0, dt, 0, 0],
                 [0, 1, 0, 0, dt, 0],
                 [0, 0, 1, 0, 0, dt],
                 [0, 0, 0, 1, 0, 0],
@@ -45,7 +46,7 @@ class KalmanFilter:
 
         # Observation matrix - We are only observing (measuring) x, y, and theta
         self.H = matrix([
-            [1, 0, 0, 0, 0, 0],
+            [1., 0, 0, 0, 0, 0],
             [0, 1, 0, 0, 0, 0],
             [0, 0, 1, 0, 0, 0],
         ])
@@ -60,18 +61,46 @@ class KalmanFilter:
         self.R = matrix(numpy.eye(self.H.shape[0])) * R
 
         # Process error covariance
-        self.Q = matrix(numpy.eye(self.A.shape[0])) * Q
+        self.Q = matrix(numpy.eye(self.A.shape[0])) * 0.001
+
+        # ADAPTIVE FILTER VARIABLES
+
+        # Enable/disable
+        self.adaptive = adaptive
+
+        # Number of standard deviations for adaptive filter to kick in
+        self.std = std
+
+        # Count for times Q has been incremented for the adaptive filter
+        self.count = 0
+
+        # How much to scale Q by for adaptive filter
+        self.Q_scale = 1000
 
     def addMeasurement(self, measurement, accVector):
         U = matrix(accVector)  # Control input vector
-        Xp = self.A * self.X0 + self.B * U
-        P = self.A * self.P0 * self.AT + self.Q
-        Y = matrix(measurement) - self.H * Xp
-        S = self.H * P * self.HT + self.R
-        K = P * self.HT * numpy.linalg.pinv(S)
-        self.X0 = Xp + K * Y
-        self.P0 = (self.I - K * self.H) * P
+        Xp = self.A * self.X0 + self.B * U  # Mathematical prediction
+        P = self.A * self.P0 * self.AT + self.Q  # Error in mathematical prediction
+        Y = matrix(measurement) - self.H * Xp  # Innovation (measurement - prediction)
+        S = self.H * P * self.HT + self.R  # Innovation covariance
+        K = P * self.HT * numpy.linalg.pinv(S)  # Kalman gain (how much to weight prediction vs measurement)
+        self.X0 = Xp + K * Y  # New state matrix
+        self.P0 = (self.I - K * self.H) * P  # New predicted error covariance
+
+        # Increase Q if measurement was way off (or decrease if the measurement has returned to more likely values)
+        # This should activate when the robot manuevers so that the model can correct itself quickly
+        if self.adaptive:
+            self.runAdaptiveFilter(Y, S)
         return self.X0
+
+    def runAdaptiveFilter(self, Y, S):
+        E = Y.T * S.I * Y
+        if E > self.std:
+            self.Q *= self.Q_scale
+            self.count += 1
+        elif self.count > 0:
+            self.Q /= self.Q_scale
+            self.count -= 1
 
     def printValues(self):
         vals = self.X0.tolist()
