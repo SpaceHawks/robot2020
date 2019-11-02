2d3eae5b4b2bbe293acc4c71e1422dc6d0910003
camero2734 <42698419+camero2734@users.noreply.github.com>
2019-11-02 15:38:37 -0500

    Added units to output

--- robot_src/imu.py
+++ robot_src/imu.py
@@ -48,15 +48,18 @@ class IMU:
         return value
 
     def readAccelerometer(self):
+        # m/s^2
+        g = 9.806
         acc_x = self.read_raw_data(self.ACCEL_XOUT_H)
         acc_y = self.read_raw_data(self.ACCEL_YOUT_H)
         acc_z = self.read_raw_data(self.ACCEL_ZOUT_H)
-        Ax = acc_x/16384.0
-        Ay = acc_y/16384.0
-        Az = acc_z/16384.0
+        Ax = g * acc_x/16384.0
+        Ay = g * acc_y/16384.0
+        Az = g * acc_z/16384.0
         return [Ax, Ay, Az]
 
     def readGyroscope(self):
+        # deg / s
         gyro_x = self.read_raw_data(self.GYRO_XOUT_H)
         gyro_y = self.read_raw_data(self.GYRO_YOUT_H)
         gyro_z = self.read_raw_data(self.GYRO_ZOUT_H)
