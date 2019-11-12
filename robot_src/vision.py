0521a67ec960912addb232aeb24558c7047a8b38
sblack808 <sblack2@hawk.iit.edu>
2019-11-11 20:38:24 -0600

    add trig calculations for x,y coordinate, need to fix a bug with the sign of the x value due to the output of arcsin

--- robot_src/vision.py
+++ robot_src/vision.py
@@ -1,6 +1,7 @@
 #vision.py
 from hokuyolx import HokuyoLX
 import numpy as np
+import math
 
 #superclass for Locater and Detector
 class SpacehawksHokuyoLXWrapper:
@@ -41,7 +42,20 @@ class SpacehawksHokuyoLXLocater(SpacehawksHokuyoLXWrapper):
 					looking_for_brighter = True
 					points_left -= 1
 		print(list_of_target_points)
-
+		distance_to_origin = list_of_target_points[1][1]
+		distance_to_helper = list_of_target_points[0][1]
+		angle_difference = abs(list_of_target_points[1][0] - list_of_target_points[0][0])
+		stripe_width = math.sqrt((distance_to_origin**2)+(distance_to_helper**2)-(2*distance_to_helper*distance_to_origin*math.cos(angle_difference)))
+		print(stripe_width)
+		origin_angle = abs(math.asin((distance_to_helper*math.sin(angle_difference))/stripe_width)) #fix me (asin gives value b/w pi/2 and -pi/2) 
+		x_factor = -1 
+		print(origin_angle)
+		if origin_angle > (math.pi)/2:
+			x_factor = 1
+			origin_angle = math.pi - origin_angle
+		x_coordinate = distance_to_origin*math.cos(origin_angle) * x_factor
+		y_coordinate = distance_to_origin*math.sin(origin_angle)
+		print(x_coordinate, y_coordinate)
 
 	def getX(self):
 		return self.x_coordinate
@@ -57,5 +71,5 @@ class SpacehawksHokuyoLXDetector(SpacehawksHokuyoLXWrapper):
 	pass
 
 #test code
-# variable = SpacehawksHokuyoLXLocater()
-# variable.update()
+variable = SpacehawksHokuyoLXLocater()
+variable.update()
