6072cece791919d2a8c94fd745d7ee66629ed9aa
camero2734 <42698419+camero2734@users.noreply.github.com>
2020-01-22 14:38:09 -0600

    Update websocket_server.py

--- robot_src/websocket_server.py
+++ robot_src/websocket_server.py
@@ -1,11 +1,14 @@
 import asyncio
 import websockets
 
+# websocket;
+
 async def response(websocket, path):
 	async for msg in websocket:
 		print(f"Received message: {msg}")
-		await websocket.send("This is a response from the server.")
+		await websocket.send("The server received your data.")
 
-start_server = websockets.serve(response, "localhost", 8080)
+# The port can be set to whatever, as long as it's open
+start_server = websockets.serve(response, port=8080)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
