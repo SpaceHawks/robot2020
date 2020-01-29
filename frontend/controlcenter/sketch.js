a608105112da1125a69e3a8c6909e83e22eb7c50
camero2734 <42698419+camero2734@users.noreply.github.com>
2020-01-28 22:30:34 -0600

    Added a bit to the control center

--- frontend/controlcenter/sketch.js
+++ frontend/controlcenter/sketch.js
@@ -1,41 +1,69 @@
 let ws;
 let settings;
+let state = 2;
+const states = ["Tank Drive", "Arcade Drive", "Autonomous"];
+const shortStates = ["TD", "AD", "AI"];
 
 let msgs = [];
 
 
 ws = {send: console.log};
 
+async function sendState() {
+	if (state < 2) await sendXBOX(states[state].split(" ").map(n => n[0]).join(""));
+	await new Promise(next => setTimeout(next, 1500));
+	sendState();
+}
+
 function setup() {
 	// ws = new WebSocket("ws://:8080");
 	// ws.onmessage = msg => {
     // 	msgs.push(msg.data);
 	// };
-	settings = QuickSettings.create(document.body.clientWidth - 300, document.body.clientHeight/2, "Robot settings")
-		.addButton("Tank Drive", ()=>sendXBOX("TD"))
-		.addButton("Arcade Drive", ()=>sendXBOX("AD"))
-		.addBoolean("Autonomous", true, () => send("AI"))
-		.addButton("STOP", () => send("STOP"))
+	settings = QuickSettings.create(document.body.clientWidth - 300, document.body.clientHeight/2, "Drive settings")
+		.addButton("Tank Drive", ()=>handlePress("TD"))
+		.addButton("Arcade Drive", ()=>handlePress("AD"))
+		.addButton("Autonomous", () => handlePress("AI"))
+		.addButton("STOP", () => handlePress("STOP"))
 		.overrideStyle("STOP", "backgroundColor", "red")
 		.overrideStyle("STOP", "color", "white")
+
+	for (let state of states) settings.overrideStyle(state, "backgroundColor", "gray");
+	settings.overrideStyle(states[state], "backgroundColor", "green");
+	sendState()
+}
+
+function handlePress(name, args) {
+	if (name === "STOP") return send("STOP");
+
+	let newState = shortStates.indexOf(name) === -1 ? state : shortStates.indexOf(name);
+	if (newState === state) return;
+	handleStateChange(state, newState);
+
+}
+
+function handleStateChange(oldState, newState) {
+	settings.overrideStyle(states[oldState], "backgroundColor", "gray");
+	settings.overrideStyle(states[newState], "backgroundColor", "green");
+	state = newState;
 }
 
 async function sendXBOX(name) {
 	try {
-		let leftY = await (await fetch("http://localhost:8000/leftY")).json();
+		let leftY = 2//await (await fetch("http://localhost:8000/leftY")).json();
 
 		if (name === "AD") {
-			leftX = await (await fetch("http://localhost:8000/leftX")).json();
+			leftX = 5//await (await fetch("http://localhost:8000/leftX")).json();
 			return send("AD", `${leftY}, ${leftX}`);
 		}
 		else {
-			let right = await (await fetch("http://localhost:8000/right")).json();
+			let right = 6//await (await fetch("http://localhost:8000/right")).json();
 			return send("TD", `${leftY}, ${right}`);
 		}
 	} catch(e) {}
 }
 
-function send(name, data) {
+async function send(name, data) {
 	if (data) return ws.send(`${name}:${data}`);
 	else return ws.send(name);
 }
