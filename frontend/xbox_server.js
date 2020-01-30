8adf52bcfd91b114f9c3f409e9e54e91a3d45d82
Dustin Van Tate Testa <toast27@gmail.com>
2020-01-30 12:49:21 -0600

    do math on all stick positions

--- frontend/xbox_server.js
+++ frontend/xbox_server.js
@@ -8,19 +8,19 @@ let controller = new xbox('045e','0719', 'xbox360-wireless')
 app.get('/spacehawks-xbox', (req, res) => res.send("Server is Running"))
 
 app.get('/leftX', (req, res) => {
-  controller.on('leftX', data => res.json(data/32768))
+    controller.on('leftX', data => res.json(100 * data / 32768))
 })
 
 app.get('/leftY', (req, res) => {
-  controller.on('leftY', res.json)
+    controller.on('leftY', data => res.json(100 * data / 32768))
 })
 
 app.get('/rightX', (req, res) => {
-  controller.on('rightX', res.json)
+    controller.on('rightX', data => res.json(100 * data / 32768))
 })
 
 app.get('/rightY', (req, res) => {
-  controller.on('rightY', res.json)
+    controller.on('rightY', data => res.json(100 * data / 32768))
 })
 
 app.listen(port)
