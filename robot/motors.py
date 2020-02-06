4194e11a9aa2fd1a824d7d2ff92051989f90d8a3
Matteo Moceri <mmoceri1@hawk.iit.edu>
2020-02-06 02:03:37 -0600

    Added Trenchdigger class with basic control to motors.py

--- robot/motors.py
+++ robot/motors.py
@@ -7,6 +7,7 @@ import busio
 # each Sabertooth controls 2 motors
 l_saber = Sabertooth('/dev/ttyS1', baudrate=9600, address = 128, timeout=1000)
 r_saber = Sabertooth('/dev/ttyS1', baudrate=9600, address = 129, timeout=1000)
+TD_saber = Sabertooth('/dev/ttyS1', baudrate=9600, address = 130, timeout=1000)
 
 
 # Chassis motors
@@ -58,3 +59,15 @@ class DriveTrain:
         motors.set_motor_speed(2, right_percent)
         motors.set_motor_speed(3, right_percent)
         motor_speeds = [left_percent, left_percent, right_percent, right_percent]
+
+class Trenchdigger:
+
+    TD_speed = 0
+
+    def set_TD_speed(percent):
+        TD_saber.drive(0, percent)
+        TD_speed = percent
+
+    def TD_stop():
+        TD_saber.drive(0, 0)
+        TD_speed = 0
