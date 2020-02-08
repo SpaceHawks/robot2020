2fa3a274ebd646eefa32ba91474187900d7b231a
Cameron Haley <chaley@hawk.iit.edu>
2020-02-07 18:16:42 -0600

    Actually include the profiler

---
+++ pathfinding/py/runprofiler.py
@@ -0,0 +1,19 @@
+import cProfile, pstats, io
+def runprofiler(fnc):
+
+    """A decorator that uses cProfile to profile a function"""
+
+    def inner(*args, **kwargs):
+
+        pr = cProfile.Profile()
+        pr.enable()
+        retval = fnc(*args, **kwargs)
+        pr.disable()
+        s = io.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
+        return retval
+
+    return inner
