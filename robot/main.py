5875e594359a83dfc080df06b052bd2f7c94db96
Matteo Moceri <mmoceri1@hawk.iit.edu>
2020-03-03 20:54:47 -0600

    Added more main.py commands

--- robot/main.py
+++ robot/main.py
@@ -20,6 +20,7 @@ def receive_msg(msg, conn):
     # stop all movement
     elif cmd == 'STOP':
         motors.DriveTrain.stop()
+        motors.Trenchdigger.set_TD_speed(0)
 
     # emergency stop
     elif cmd == 'DIE':
@@ -31,6 +32,23 @@ def receive_msg(msg, conn):
         motors.DriveTrain.tank_drive(0, 0)
         conn.send("MSG: TODO: autonomous")
         print('auto command received')
+
+    # read trench digger encoder
+    elif cmd == 'ENC':
+        motors.Trenchdigger.get_encoder()
+
+    # activate hopper servo
+    elif cmd == 'SER':
+        motors.Trenchdigger.servo(90)
+
+    # read potentiometer
+    elif cmd == 'POT':
+        motors.Trenchdigger.get_pot();
+
+    #start trench digger
+    elif cmd == 'DIG':
+        motors.Trenchdigger.set_TD_speed(0.75)
+        print('trench digger active')
 
     else:
         motors.DriveTrain.tank_drive(0,0)
