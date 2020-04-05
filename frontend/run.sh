91ac176f186423bdfbfba74a8a8601fce6de8a74
Dustin Van Tate Testa <toast27@gmail.com>
2020-04-05 18:55:58 -0500

    start xbox server

--- frontend/run.sh
+++ frontend/run.sh
@@ -3,5 +3,8 @@
 # serve frontend on localhost:3030
 http-server ./controlcenter -p3030 &
 
+# run xbox server
+node ./xbox_server/xbox_server.js &
+
 # open in browser
 chromium "http://localhost:3030"
